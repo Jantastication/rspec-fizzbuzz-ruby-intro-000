fe36f2e8e947cfec9e8e1756ee5381758c6d5818
Jantastication O <georginaoregx@gmail.com>
2018-09-20 03:58:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,8 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif (int % 3 == 0 && int % 5 == 0)
+  elsif int % 3 == 0 and int % 5 == 0
     "FizzBuzz"
-  else int % 4 == 4
+  else int % 4 == int
   end
 end
